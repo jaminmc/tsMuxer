edf2819925f69a113eaf9dd76e07c6aee93b41d0
jaminmc <1310376+jaminmc@users.noreply.github.com>
2026-02-05 02:03:03 -0500

    Update Workflows for macOS

--- .github/workflows/linux_gui.sh
+++ .github/workflows/linux_gui.sh
@@ -1,13 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
 cd tsMuxerGUI
 
 mkdir qmake_build
 cd qmake_build
 qmake ..
-make -j$(nproc --all)
+make -j"$(nproc --all)"
 
 cd ../..
 
 mkdir cmake_build
 cd cmake_build
 cmake -DTSMUXER_GUI=TRUE -DQT_VERSION=6 ..
-make -j$(nproc --all) tsMuxerGUI
+make -j"$(nproc --all)" tsMuxerGUI
